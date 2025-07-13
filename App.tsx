bb67860ecc0b1d446dd29fe6a87ad6710a60be09
RAJ-MANE <mmane94@gmail.com>
2025-07-13 19:05:45 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Team from "@/pages/team";
+import Resources from "@/pages/resources";
+import Events from "@/pages/events";
+import StudentsCorner from "@/pages/students-corner";
+import Contact from "@/pages/contact";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/team" component={Team} />
+      <Route path="/resources" component={Resources} />
+      <Route path="/events" component={Events} />
+      <Route path="/students-corner" component={StudentsCorner} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
